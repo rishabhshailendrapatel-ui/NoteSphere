83cada90de14087f93cac3164c47ad8f19c8087b
Rishabh Patel <rishabhshailendrapatel@gmail.com>
2026-08-28 19:49:07 +0530

    feat (auth): add create-account API endpoint with JWT token generation

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,9 @@ const cors = require("cors");
 const connectDB = require("./config/database");
 const dotenv = require("dotenv");
 dotenv.config();
+const jwt = require("jsonwebtoken");
+const { authenticateToken } = require("./utilities");
+const User = require("./models/user.model");
 
 const app = express();
 
@@ -18,6 +21,55 @@ app.use(cors({ origin: "*" }));
 // Routes
 app.get("/", (req, res) => {
   res.json({ data: "Hello World" });
+});
+
+// Create Account
+app.post("/create-account", async (req, res) => {
+  const { fullName, email, password } = req.body;
+
+  if (!fullName) {
+    return res
+      .status(400)
+      .json({ error: true, message: "Full name is required" });
+  }
+
+  if (!email) {
+    return res.status(400).json({ error: true, message: "Email is required" });
+  }
+
+  if (!password) {
+    return res
+      .status(400)
+      .json({ error: true, message: "Password is required" });
+  }
+
+  const isUser = await User.findOne({ email: email });
+
+  if (isUser) {
+    return res.status(400).json({
+      error: true,
+      message: "User already exist",
+    });
+  }
+
+  const user = new User({
+    fullName,
+    email,
+    password,
+  });
+
+  await user.save();
+
+  const accessToken = jwt.sign({ user }, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: "3600m",
+  });
+
+  return res.json({
+    error: false,
+    user,
+    accessToken,
+    message: "Registration Successful",
+  });
 });
 
 app.listen(PORT, () => {
