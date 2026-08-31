db71a217956c5fa3d7382030690065bb06960021
Rishabh Patel <rishabhshailendrapatel@gmail.com>
2026-08-31 09:06:07 +0530

    feat (notes): add POST route for creating notes

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,7 @@ dotenv.config();
 const jwt = require("jsonwebtoken");
 const { authenticateToken } = require("./utilities");
 const User = require("./models/user.model");
+const Note = require("./models/note.model");
 
 const app = express();
 
@@ -108,6 +109,47 @@ app.post("/login", async (req, res) => {
     });
   }
 });
+
+// Add Note
+app.post("/add-note", authenticateToken, async (req, res) => {
+  const { title, content, tags } = req.body;
+  const { user } = req.user;
+
+  if(!title) {
+    return res
+    .status(400)
+    .json({ error: true, message: "Title is required" });
+  }
+
+  if(!content) {
+    return res
+    .status(400)
+    .json({ error: true, message: "Content is required" });
+  }
+
+  try {
+    const note = new Note({
+      title,
+      content,
+      tags: tags || [],
+      userId: user._id,
+    })
+
+    await note.save();
+
+    return res.json({
+      error: false,
+      note,
+      message: "Note added Successfully",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      error: true,
+      message: "Internal server Error",
+    });
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on address http://localhost:${PORT}`);
 });
